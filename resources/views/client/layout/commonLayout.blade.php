67f6b8935fbb4bd6cdf7c3b42e6b97ff19079637
MahesaSyawalA <mahesaabd4@gmail.com>
2025-03-17 14:37:15 +0700

    adjustment : copyright footer

--- resources/views/client/layout/commonLayout.blade.php
+++ resources/views/client/layout/commonLayout.blade.php
@@ -93,7 +93,7 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-12 footer-copyright text-center">
-                            <p class="mb-0">Copyright <span class="year-update"> </span> © Cuba Theme By Pixelstrap
+                            <p class="mb-0">Copyright <span class="year-update"> </span> © SPTH
                             </p>
                         </div>
                     </div>
