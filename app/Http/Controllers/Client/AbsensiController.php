f662398a32fd9e77cea78ec48383f5854777fd14
hanz-fr <raihanf787@gmail.com>
2025-03-19 16:03:18 +0700

    refactor: few changes in absensicontroller and scanqr page, experimenting

--- app/Http/Controllers/Client/AbsensiController.php
+++ app/Http/Controllers/Client/AbsensiController.php
@@ -2,6 +2,8 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AbsensiController extends Controller
 {
@@ -14,5 +16,14 @@ class AbsensiController extends Controller
 
     public function absen() {
         return view('client.scanqr');
+    }
+
+    public function validasiAbsen(Request $request) {
+        $request->validate([
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric'
+        ]);
+
+        $user = Auth::user();
     }
 }
--- resources/views/client/scanqr.blade.php
+++ resources/views/client/scanqr.blade.php
@@ -7,7 +7,42 @@
 @endsection
 
 @section('content')
-<div>test</div>
+<h2>Scan QR to Check Attendance</h2>
+    <button onclick="scanQRCode()">Scan QR Code</button>
+
+    <script>
+        function scanQRCode() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        let latitude = position.coords.latitude;
+                        let longitude = position.coords.longitude;
+
+                        // Send to Laravel backend
+                        fetch('/api/attendance', {
+                            method: 'POST',
+                            headers: { 
+                                'Content-Type': 'application/json',
+                                'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                            },
+                            body: JSON.stringify({
+                                latitude: latitude,
+                                longitude: longitude
+                            })
+                        })
+                        .then(response => response.json())
+                        .then(data => alert(data.message))
+                        .catch(error => console.error('Error:', error));
+                    },
+                    (error) => {
+                        alert('Error getting location: ' + error.message);
+                    }
+                );
+            } else {
+                alert("Geolocation is not supported by this browser.");
+            }
+        }
+    </script>
 @endsection
 
 @section('scriptPlugins')
