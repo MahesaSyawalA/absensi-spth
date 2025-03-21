8cbca3abf3f8069bdc3bb643d30691bfbc196e6f
MahesaSyawalA <mahesaabd4@gmail.com>
2025-03-21 20:40:30 +0700

    integration : navbar and profile page

--- app/Http/Controllers/Client/ProfileController.php
+++ app/Http/Controllers/Client/ProfileController.php
@@ -2,12 +2,15 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function index(){
+        $user = Auth::user();
         $data = [
             'title' => 'Profile',
+            'user' => $user,
         ];
         return view('client.profile',$data);
     }
--- resources/views/client/profile.blade.php
+++ resources/views/client/profile.blade.php
@@ -15,23 +15,16 @@
                 <div class="user-image">
                     <div class="avatar">
                         <div class="common-align">
-                            <div><img id="output" src="../assets/images/dashboard-11/user/12.jpg" alt="Profile Image">
-                                <input type="file" accept="image/*" onchange="loadFile(event)">
+                            <div>
+                                <img id="output" src="{{$user->foto}}" alt="Profile Image" style="min-height: 80px;">
+                                {{-- <input type="file" accept="image/*" onchange="loadFile(event)">
                                 <div class="icon-wrapper" id="cancelButton"><i class="icofont icofont-error"></i></div>
-                                <div class="icon-wrapper"><i class="icofont icofont-pencil-alt-5"></i></div>
+                                <div class="icon-wrapper"><i class="icofont icofont-pencil-alt-5"></i></div> --}}
                             </div>
-                            <div class="user-designation"><a target="_blank" href="">Ade Irfan Yulianto</a>
-                                <div class="desc">Driver</div>
+                            <div class="user-designation"><a target="_blank" href="">{{$user->nama}}</a>
+                                <div class="desc">{{$user->jabatan}}</div>
                             </div>
                         </div>
-                        {{-- <div class="follow">
-                            <div>
-                                <div class="follow-num counter" data-target="258690">0</div><span>Follower</span>
-                            </div>
-                            <div>
-                                <div class="follow-num counter" data-target="659887">0</div><span>Following</span>
-                            </div>
-                        </div> --}}
                     </div>
                 </div>
             </div>
@@ -41,7 +34,7 @@
                 <div class="row g-3">
                     <div class="col-lg-3 col-md-4 col-sm-6">
                         <div class="ttl-info text-start">
-                            <h6><i class="icon-id-badge"></i> Nip</h6><span>000000000000000001</span>
+                            <h6><i class="icon-id-badge"></i> Nip</h6><span>{{$user->nip}}</span>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-4 col-sm-6">
@@ -51,7 +44,7 @@
                     </div>
                     <div class="col-lg-3 col-md-4 col-sm-6">
                         <div class="ttl-info text-start">
-                            <h6><i class="fa-solid fa-calendar-days pe-2"></i>Tanggal Lahir</h6><span>1995-06-19</span>
+                            <h6><i class="fa-solid fa-calendar-days pe-2"></i>Tanggal Lahir</h6><span>{{$user->tanggal_lahir}}</span>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-4 col-sm-6">
