ef6b2b8f3e3827f2614eef4dab91c834734d8800
MahesaSyawalA <mahesaabd4@gmail.com>
2025-03-21 19:20:29 +0700

    feat : integaration auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,8 @@ class User extends Authenticatable
         'status_pegawai',
         'foto',
         'role',
+        'username',
+        'password',
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,40 @@ use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\SanitizeInput;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'index']);
-Route::get('/penilaian-staff',[HomeController::class,'indexPenilaian']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/penilaian-staff', [HomeController::class, 'indexPenilaian']);
+
+Route::get('/login', [AuthSessionController::class, 'index'])->name('login');
+
+// Route untuk proses login
+Route::post('/login', [AuthSessionController::class, 'login'])->name('login.post');
+
+// Route untuk proses logout
+Route::post('/logout', [AuthSessionController::class, 'logout'])->name('logout');
+Route::get('/check-login', [AuthSessionController::class, 'checkLoginStatus']);
 
 
-Route::get('/login', [AuthSessionController::class, 'index'])->name('auth.index');
+// Route yang dilindungi auth
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('/admin/management-user', [UserController::class, 'index'])->name('management-user');
+    Route::post('/users', [UserController::class, 'store'])->middleware('sanitize')->name('user.store');
+    Route::get('/users/{nip}', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{nip}', [UserController::class, 'update'])->middleware('sanitize')->name('users.update');
+    Route::delete('/users/{nip}', [UserController::class, 'destroy'])->name('users.destroy');
+    // Route::get('/admin', [AdminController::class,'index']);
 
-// Route::get('/admin', [AdminController::class,'index']);
-Route::get('/admin/management-user', [UserController::class, 'index'])->name('user.index');
 
-Route::post('/users', [UserController::class, 'store'])->middleware('sanitize')->name('user.store');
-Route::get('/users/{nip}', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{nip}', [UserController::class, 'update'])->middleware('sanitize')->name('users.update');
-Route::delete('/users/{nip}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::post('/kriteria', [KriteriaPenilaianController::class, 'store'])->middleware('sanitize')->name('kriteria.store');
+    Route::delete('/kriteria/{id}', [KriteriaPenilaianController::class, 'destroy'])->name('kriteria.destroy');
+    Route::post('/sub-kriteria', [KriteriaPenilaianController::class, 'storeSubKriteria'])->middleware('sanitize')->name('subKriteria.store');
+    Route::delete('/sub-kriteria/{id}', [KriteriaPenilaianController::class, 'destroySubKriteria'])->name('subKriteria.destroy');
 
-Route::post('/kriteria', [KriteriaPenilaianController::class, 'store'])->middleware('sanitize')->name('kriteria.store');
-Route::delete('/kriteria/{id}', [KriteriaPenilaianController::class, 'destroy'])->name('kriteria.destroy');
-Route::post('/sub-kriteria', [KriteriaPenilaianController::class, 'storeSubKriteria'])->middleware('sanitize')->name('subKriteria.store');
-Route::delete('/sub-kriteria/{id}', [KriteriaPenilaianController::class, 'destroySubKriteria'])->name('subKriteria.destroy');
+    Route::get('/admin/rekap-penilaian', [RekapPenilaianController::class, 'index'])->name('admin.index');
+    Route::get('/admin/kriteria-penilaian', [KriteriaPenilaianController::class, 'index']);
+});
 
-Route::get('/admin/rekap-penilaian', [RekapPenilaianController::class, 'index']);
-Route::get('/admin/kriteria-penilaian', [KriteriaPenilaianController::class, 'index']);
-
-Route::get('/staff/absensi', [AbsensiController::class, 'index']);
-Route::get('/staff/absen-scan', [AbsensiController::class, 'absen']);
-Route::get('/staff/profile', [ProfileController::class, 'index']);
+Route::middleware(['auth', 'is_pegawai'])->group(function () {
+    Route::get('/staff/absensi', [AbsensiController::class, 'index']);
+    Route::get('/staff/absen-scan', [AbsensiController::class, 'absen']);
+    Route::get('/staff/profile', [ProfileController::class, 'index']);
+});
